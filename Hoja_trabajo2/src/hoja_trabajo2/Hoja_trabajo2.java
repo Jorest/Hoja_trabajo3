e0493db492e46c83a466b2bc03c4617c47b8ba7a
Jorge <Jorge@192.168.1.2>
2014-07-18 07:49:52 -0600

    creacion del archivo

---
+++ Hoja_trabajo2/src/hoja_trabajo2/Hoja_trabajo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja_trabajo2;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Hoja_trabajo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
